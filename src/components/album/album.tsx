f5466e710a31d45062bf4dffed0d1f1fc81af4c2
Wesley Mendes <wesleymendes123321@gmail.com>
2023-07-20 13:58:07 -0300

    [Wesley Mendes] - Requisito 6 (5 quebrou)

--- src/components/album/album.tsx
+++ src/components/album/album.tsx
@@ -4,16 +4,28 @@ import Loading from '../loading/loading';
 import getMusics from '../../services/musicsAPI';
 import { AlbumType, SongType } from '../../types';
 import MusicCard from '../MusicCard/MusiCard';
+import { addSong, removeSong } from '../../services/favoriteSongsAPI';
 
 function Album() {
   const [loading, setLoading] = useState(false);
   const [musicData, setMusicData] = useState<(AlbumType | SongType)[]>([]);
+  const [srcImage, setSrcImage] = useState('/src/images/empty_heart.png');
+
+  const emptyHeartImage = '/src/images/empty_heart.png';
+  const checkedHeartImage = '/src/images/checked_heart.png';
 
   const { id } = useParams();
 
   const handleFavorite = (checked: boolean, trackId: number) => {
     setMusicData(musicData.map((music) => {
       if ((music as SongType).trackId === trackId) {
+        if (!checked) {
+          removeSong(music as SongType);
+          console.log('foi remove song');
+        } else if (checked) {
+          addSong(music as SongType);
+          console.log('foi add song');
+        }
         return { ...music, checked };
       }
       return music;
@@ -45,16 +57,35 @@ function Album() {
           <h2 data-testid="album-name">
             { (musicData[0] as AlbumType).collectionName }
           </h2>
-          { musicData.slice(1).map((music) => (
-            <MusicCard
-              key={ (music as SongType).trackId }
-              trackId={ (music as SongType).trackId }
-              trackName={ (music as SongType).trackName }
-              previewUrl={ (music as SongType).previewUrl }
-              checked={ (music as SongType).checked }
-              handleFavorite={ handleFavorite }
-            />
-          )) }
+          { musicData.slice(1).map((music) => {
+            const { trackId, trackName, previewUrl, checked } = music as SongType;
+            return (
+              <div key={ trackId }>
+
+                <p>{ trackName }</p>
+                <audio
+                  data-testid="audio-component"
+                  src={ previewUrl }
+                  controls
+                >
+                  <track kind="captions" />
+                  O seu navegador não suporta o elemento
+                  <code>audio</code>
+                </audio>
+                <label data-testid={ `checkbox-music-${trackId}` }>
+                  <img
+                    src={ checked ? checkedHeartImage : emptyHeartImage }
+                    alt="Empty Heart"
+                  />
+                  <input
+                    onChange={ ({ target }) => handleFavorite(target.checked, trackId) }
+                    id="favoriteCheck"
+                    type="checkbox"
+                  />
+                </label>
+              </div>
+            );
+          }) }
         </div>
       ) }
     </>
